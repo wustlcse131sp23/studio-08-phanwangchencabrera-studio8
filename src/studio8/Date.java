8bd9d8d6761092e6289dc687adafc5595309ff8e
cassiephan <cassiephan@localhost>
2023-04-06 12:40:43 -0500

    Finished kinda

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,89 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+
 public class Date {
-
-
+	
+	private String monthName;
+	private int day;
+	private int year;
+	private boolean isHoliday;
+	
+	/**
+	 * 
+	 * @param monthName
+	 * @param day
+	 * @param year
+	 * @param isHoliday
+	 */
+	public Date(String monthName, int day, int year, boolean isHoliday) {
+		this.monthName = monthName;
+		this.day = day;
+		this.year = year;
+		this.isHoliday = isHoliday;
+	}
+	
+//	/**
+//	 * 
+//	 * @return
+//	 */
+//	public String getMonthName() {
+//		return monthName;
+//	}
+//	
+//	/**
+//	 * 
+//	 * @return
+//	 */
+//	public int getDay() {
+//		return day;
+//	}
+//	
+//	/**
+//	 * 
+//	 * @return
+//	 */
+//	public int getYear() {
+//		return year;
+//	}
+//	
+//	/**
+//	 * 
+//	 * @return
+//	 */
+//	public boolean isHoliday() {
+//		return isHoliday;
+//	}
+	 public String toString() {
+		 return this.monthName + " " + this.day + ", " + this.year + " " + this.isHoliday;
+	    }
+	
     public static void main(String[] args) {
+    	Date d1 = new Date("February", 14, 2023, true);
+    	Date d2 = new Date("March", 2, 2023, false);
+    	Date d3 = new Date("January", 1, 2023, true);
+    	Date d4 = new Date("April", 8, 2004, false);
+    	Date d5 = new Date("April", 8, 2004, false);
+    	
+    	LinkedList<Date> list = new LinkedList<Date>();
+    	list.add(d1);
+    	list.add(d2);
+    	list.add(d3);
+    	list.add(d4);
+    	list.add(d5);
+    	System.out.println(list);
+    	
+    	HashSet<Date> set = new HashSet<Date>();
+    	set.add(d1);
+    	set.add(d2);
+    	set.add(d3);
+    	set.add(d4);
+    	set.add(d4);
+    	System.out.println(set);
 
     }
-
+    
 }
+
+	
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,57 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public class Time {
 
+	private int hour;
+	private String minute;
+	private boolean isTwelve;
+
+	public Time(int hour, String minute, boolean isTwelve) {
+		this.hour = hour;
+		this.minute = minute;
+		this.isTwelve = isTwelve;
+	}
+	
+	public int isTwelve() {
+		if (true) {
+			if (hour > 12) {
+				return hour - 12;
+				
+			}
+		}
+		return hour;
+	}
+
+	public String toString() {
+		if(isTwelve) {
+			if (hour > 12) {
+				return (hour - 12) + ":" + this.minute + "pm";	
+		}
+			return this.hour + ":" + this.minute + "am";
+	}
+		return this.hour + ":" + this.minute;
+	}
+
 	public static void main(String[] args) {
- 
-    	
-    }
+		Time t1 = new Time(23, "00", true);
+		Time t2 = new Time(12, "40", false);
+		Time t3 = new Time(4, "12", false);
+
+		LinkedList<Time> list = new LinkedList<Time>();
+		list.add(t1);
+		list.add(t2);
+		list.add(t3);
+		System.out.println(list);
+
+		HashSet<Time> set = new HashSet<Time>();
+		set.add(t1);
+		set.add(t2);
+		set.add(t2);
+		System.out.println(set);
+
+	}
 
 }
